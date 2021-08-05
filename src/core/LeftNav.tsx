1bab6ddd97fb9eb35620feb11e7b1db15e4ab0ab
Terence Qu <therealterencequ@gmail.com>
2021-08-05 15:02:15 +1200

    Skeleton for navigations

---
+++ src/core/LeftNav.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Drawer} from "@material-ui/core";
+
+const LeftNav = () => {
+  return <Drawer>
+    
+  </Drawer>
+}
+
+export default LeftNav;
---
+++ src/core/TopNav.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {AppBar} from "@material-ui/core";
+
+const TopNav = () => {
+  return <AppBar>
+
+  </AppBar>
+}
+
+export default TopNav;
