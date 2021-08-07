a9af9c6ecc16e485b8462f231f338a0b1d007191
Terence <terenco12345@gmail.com>
2021-08-08 01:08:59 +1200

    remove comment

--- src/core/pages/CharacterPage.tsx
+++ src/core/pages/CharacterPage.tsx
@@ -7,7 +7,7 @@ type TParams = { id: string; };
 
 const CharacterPage: FC = () => {
 
-  //const { id } = useParams<TParams>(); // I have commented this out because an unused variable makes the pipeline break
+  const { id } = useParams<TParams>();
 
   return <>
     <Typography variant={"h1"}>CharacterPage</Typography>
