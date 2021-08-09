0866da664395ac555a67a8d9efcaaa211fcf8596
Terence <terenco12345@gmail.com>
2021-08-09 16:08:17 +1200

    Set up Redux-Persist

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from "./reportWebVitals";
 import {SetupGlobalAxiosInterceptor} from "./api/interceptors/HttpInterceptor";
 import {Provider} from "react-redux";
-import {store} from "./redux/store";
+import {persistor, store} from "./redux/store";
+import {PersistGate} from "redux-persist/integration/react";
 
 // Axios
 SetupGlobalAxiosInterceptor();
@@ -14,7 +15,9 @@ SetupGlobalAxiosInterceptor();
 render(
   <React.StrictMode>
       <Provider store={store}>
-        <App/>
+          <PersistGate persistor={persistor}>
+              <App/>
+          </PersistGate>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
