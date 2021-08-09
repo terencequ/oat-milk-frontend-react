c51b2688029be80698c82a3ae78c739255022853
Terence <terenco12345@gmail.com>
2021-08-09 16:10:28 +1200

    Move JWT retrieval to inside interceptor method

--- src/api/interceptors/HttpInterceptor.ts
+++ src/api/interceptors/HttpInterceptor.ts
@@ -6,11 +6,11 @@ import {store} from "../../redux/store";
  */
 export function SetupGlobalAxiosInterceptor(){
     const baseUrl = process.env.REACT_APP_API_URL;
-    const authJwt = store.getState().users.authToken;
     console.log("set up global axios");
     globalAxios.interceptors.request.use(
         async config => {
             config.baseURL = baseUrl;
+            const authJwt = store.getState().users.authToken;
             if(authJwt != null){
                 config.headers["Authorization"] = `Bearer ${authJwt}`;
             }
